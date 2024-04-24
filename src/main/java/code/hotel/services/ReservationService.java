fe97c9f1dddc0b6710ae3055a30f99a54287cc36
PKV11 <92929887+PKV11@users.noreply.github.com>
2024-04-24 14:20:32 +0530

    Add files via upload

---
+++ src/main/java/code/hotel/services/ReservationService.java
@@ -0,0 +1,27 @@
+package henry.hotel.services;
+
+import java.util.Collection;
+
+import henry.hotel.entity.Reservation;
+import henry.hotel.temp.CurrentReservation;
+
+//Service Pattern for Reservation
+public interface ReservationService {
+
+	public Reservation getReservationForLoggedUserById(int resId);
+
+	public Collection<Reservation> getReservationsForLoggedUser();
+
+	public void saveOrUpdateReservation(CurrentReservation currentReservation);
+
+	public void deleteReservation(int resId);
+
+	public CurrentReservation reservationToCurrentReservationById(int resId);
+
+	public void bookRoom(CurrentReservation currentReservation);
+
+	void checkInReservation(int reservationId);
+
+	void checkOutReservation(int reservationId);
+
+}
